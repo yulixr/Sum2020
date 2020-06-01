c01b56328d773dfd33b8cf220514664aaad1f24f
yulixr <raksinay@mail.ru>
2020-06-01 17:50:05 +0300

    Completed eyes programm

---
+++ T02EYES/T02EYES.C
@@ -0,0 +1,158 @@
+/* Raksina Yulia, 01.06.2020 */
+#include <windows.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define WND_CLASS_NAME "My Class Name"
+
+/* Forward declaration */
+LRESULT CALLBACK WinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+
+/* The main programm func */
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT CmdShow )
+{
+  HWND hWnd;
+  MSG msg;
+  WNDCLASS wc;
+
+  /* Fill window class structure */
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_SHIELD);
+  wc.hInstance = hInstance;
+  wc.lpszClassName = WND_CLASS_NAME;
+  wc.lpszMenuName = NULL;
+  wc.lpfnWndProc = WinFunc;
+
+  /* Register window class */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "Error", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Window creation */
+  hWnd = CreateWindow(WND_CLASS_NAME, "Practice", WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+    NULL, NULL, hInstance, NULL);
+
+  ShowWindow(hWnd, CmdShow);
+  
+  /* Message loop */
+  while (GetMessage(&msg, NULL, 0, 0))
+    DispatchMessage(&msg);
+
+  return 0;
+} /* End of 'WinMain' func */
+
+/* Draw Eye func */
+void DrawEye( HDC hDC, INT X, INT Y, INT R, INT R1, INT Mx, INT My )
+{
+  INT dx = Mx - X, dy = My - Y, d = R - R1;
+  DOUBLE len = sqrt( dx * dx + dy * dy), k;
+
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SelectObject(hDC, GetStockObject(DC_PEN));
+  SetDCBrushColor(hDC, RGB(255, 255, 255));
+  SetDCPenColor(hDC, RGB(0, 0, 0)); 
+
+  Ellipse(hDC, X - R, Y - R, X + R + 1, Y + R + 1);
+
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SelectObject(hDC, GetStockObject(DC_PEN));
+  SetDCBrushColor(hDC, RGB(100, 160, 255));
+  SetDCPenColor(hDC, RGB(0, 0, 255)); 
+
+  if (len == 0)
+    k = 0;
+  else if (d > len)
+    k = len / d;
+  else
+    k = d / len;
+  dx *= k;
+  dy *= k;
+
+  Ellipse(hDC, X + dx - R1, Y + dy - R1,
+    X + dx + R1 + 1, Y + dy + R1 + 1); 
+}/* End Draw eye func */
+
+/* Main window handle func */
+LRESULT CALLBACK WinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
+{
+  HDC hDC;
+  PAINTSTRUCT ps;
+  POINT pt;
+  HPEN hPen, hOldPen;
+  static INT w, h;
+  static HDC hMemDC;
+  static HBITMAP hBm;
+  INT i, j, r, r1;
+  
+  switch(Msg)
+  {
+  case WM_CREATE:
+    SetTimer(hWnd, 30, 60, NULL);
+    hDC = GetDC(hWnd);
+    hMemDC = CreateCompatibleDC(hDC);
+    ReleaseDC(hWnd, hDC);
+    hBm = NULL;
+    return 0;
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+    SelectObject(hMemDC, hBm);
+    //SendMessage(hWnd, WM_TIMER, 0, 0);
+    return 0;
+  case WM_KEYDOWN:
+    if (wParam == VK_ESCAPE)
+      SendMessage(hWnd, WM_CLOSE, 0, 0);
+    return 0;
+  case WM_TIMER:
+    /* cleaning background */
+    SelectObject(hMemDC, GetStockObject(WHITE_BRUSH));
+    SelectObject(hMemDC, GetStockObject(NULL_PEN));
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1);
+
+    GetCursorPos(&pt);
+    ScreenToClient(hWnd, &pt);
+    /* calling draw eye func */
+    srand(111);
+    r = 55;
+    r1 = 16;
+    for(i = r; i < h; i += 2 * r + rand() % 31 + 15)
+      for(j = r; j < w; j += 2 * r + rand() % 49 + 20)
+        DrawEye(hMemDC, j, i, r + rand() % 13, r1, pt.x, pt.y); 
+
+    InvalidateRect(hWnd, NULL, FALSE);
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+    EndPaint(hWnd, &ps);
+    return 0;
+  case WM_CLOSE:
+    if(MessageBox(hWnd, "Are you sure you want to close?", "Close", 
+        MB_YESNO | MB_DEFBUTTON2 | MB_ICONQUESTION) == IDYES)
+      DestroyWindow(hWnd);
+    return 0;
+  case WM_DESTROY:
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    DeleteDC(hMemDC);
+    PostMessage(hWnd, WM_QUIT, 0, 0);
+    KillTimer(hWnd, 30);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+}/* End of 'WinFunc' func */
+
+/* END OF 'T01FWIN.C' FILE */
