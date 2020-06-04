926004611bf901f562ee8488a81d7d81368a9de8
yulixr <raksinay@mail.ru>
2020-06-04 15:32:53 +0300

    Drawing polygonal globe

---
+++ T07GLOBE/GLOBE.C
@@ -0,0 +1,159 @@
+/* YR4, 04.06.2020 */
+#include "GLOBE.H"
+#include <time.h>
+
+VEC G[GLOBE_H][GLOBE_W];
+
+/* Store coord syst center */
+
+static INT CenterX, CenterY;
+
+/* Rotate vector around z axis func
+ * ARGUMENTS:
+ *   -vector to rotate
+ *      VEC V;
+ *   -rotation angle in degrees:
+ *      DBL Angle;
+ * RETURNS:
+ *   (VEC) rotated vector
+ */
+VEC VecRotateZ( VEC V, DBL Angle )
+{
+  VEC r;
+  
+  Angle *= PI / 180; 
+  r.X = V.X * cos(Angle) - V.Y * sin(Angle);
+  r.Y = V.X * sin(Angle) + V.Y * cos(Angle);
+  r.Z = V.Z;
+
+  return r;
+}
+
+VEC VecRotateY(VEC V, DOUBLE Angle)
+{
+    DOUBLE a = Angle * PI / 180, si = sin(a), co = cos(a);
+    VEC r;
+
+    r.X = V.X * co - V.Y * si;
+    r.Y = V.X * si + V.Y * co;
+    r.Z = V.Z;
+
+    return r;
+}
+
+VEC VecRotateX(VEC V, DOUBLE Angle)
+{
+    DOUBLE a = Angle * PI / 180, si = sin(a), co = cos(a);
+    VEC r;
+
+    r.Y = V.Y  * co - V.Z * si ;
+    r.Z = V.Y * si + V.Z * co;
+    r.X = V.X;
+
+    return r;
+}
+
+
+
+/* Fill geometry func
+ * ARGUMENTS:
+ *   -Coordinates
+ *      DBL Xc, DBL Yc; 
+ *   -rotation angle in degrees:
+ *      DBL Angle;
+ * RETURNS:
+ *   (VEC) rotated vector
+ */
+VOID GlobeSet( DBL Xc, DBL Yc, DBL R )
+{
+  INT i, j;
+  DOUBLE t, p;
+
+  CenterX = Xc, CenterY = Yc;
+  for (i = 0, t = 0; i < GLOBE_H; i++, t += PI / (GLOBE_H - 1))
+    for (j = 0, p = 0; j < GLOBE_W; j++, p += 2 * PI / (GLOBE_W - 1))
+     { 
+        G[i][j].X = R * cos(p) * sin(t);
+        G[i][j].Y =  R * cos(t);
+        G[i][j].Z = R * sin(p) * sin(t);
+     }
+} /* end globeset func */
+
+/* Draw Globe func
+ * ARGUMENTS:
+ *   -hDC
+ *      HDC hDC;
+ */
+VOID GlobeDraw( HDC hDC )
+{
+  DBL s = 5, k;
+  INT i, j;
+  DBL t = (DBL)clock() / CLOCKS_PER_SEC;
+  DBL z[GLOBE_H][GLOBE_W];
+  static POINT pnt[GLOBE_H][GLOBE_W];
+   
+  /* draw using dots */
+
+  for (i = 0; i < GLOBE_H; i++)
+    for (j = 0; j < GLOBE_W; j++)
+    {
+       VEC 
+         v1 = VecRotateY(G[i][j], 20 * t),
+         v = VecRotateX(v1, 10 * t);
+       
+       z[i][j] = v.Z;
+       pnt[i][j].x = CenterX + (INT)v.X,
+       pnt[i][j].y = CenterY - (INT)v.Y; 
+    }
+  
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SetDCBrushColor(hDC, RGB(0, 0, 0));
+  for (i = 0; i < GLOBE_H; i++)
+    for (j = 0; j < GLOBE_W; j++)
+     if (z[i][j] > 0)
+        //Ellipse(hDC, pnt[i][j].x - s, pnt[i][j].y - s, pnt[i][j].x + s, pnt[i][j].y + s); 
+  
+  /* linear draw */
+  /*
+  for (i = 0; i < GLOBE_H; i++)
+  { 
+    MoveToEx(hDC, pnt[i][0].x, pnt[i][0].y, NULL);
+    for (j = 1; j < GLOBE_W; j++)
+    {       
+      LineTo(hDC, pnt[i][j].x, pnt[i][j].y);
+    }
+  } 
+  for (j = 0; j < GLOBE_W; j++)
+  { 
+    MoveToEx(hDC, pnt[0][j].x, pnt[0][j].y, NULL);
+    for (i = 1; i < GLOBE_H; i++)
+    {       
+      LineTo(hDC, pnt[i][j].x, pnt[i][j].y);
+    }
+  }*/
+
+  /* polygon draw */
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SelectObject(hDC, GetStockObject(DC_PEN));
+  srand(30);
+  for (i = 0; i < GLOBE_H - 1; i++)
+    for (j = 0; j < GLOBE_W - 1; j++)
+      { 
+        POINT ps[4];
+        ps[0] = pnt[i][j]; 
+        ps[1] = pnt[i][j + 1]; 
+        ps[2] = pnt[i + 1][j + 1]; 
+        ps[3] = pnt[i + 1][j]; 
+
+        k = ((ps[0].x - ps[1].x) * (ps[0].y + ps[1].y) +
+             (ps[1].x - ps[2].x) * (ps[1].y + ps[2].y) +
+             (ps[2].x - ps[3].x) * (ps[2].y + ps[3].y) +
+             (ps[3].x - ps[0].x) * (ps[3].y + ps[0].y));
+        if (k >= 0)
+        {
+        SetDCBrushColor(hDC, RGB(100, 150, 0));
+        SetDCPenColor(hDC, RGB(50, 50, 0));
+        Polygon(hDC, ps, 4);
+        }
+      } 
+} /*end drawglobe func */
---
+++ T07GLOBE/T07GLOBE.C
@@ -0,0 +1,182 @@
+/* FILE NAME: T07GLOBE.C
+   PROGRAMMER: YR4
+   DATE: 04.01.2020
+   PURPOSE: Globe
+*/
+
+#include "GLOBE.H"
+
+#define WND_CLASS_NAME "My Class Name"
+
+
+/* Forward declaration */
+LRESULT CALLBACK WinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+
+/* The main programm func */
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT CmdShow )
+{
+  HWND hWnd;
+  MSG msg;
+  WNDCLASS wc;
+
+  /* Fill window class structure */
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_SHIELD);
+  wc.hInstance = hInstance;
+  wc.lpszClassName = WND_CLASS_NAME;
+  wc.lpszMenuName = NULL;
+  wc.lpfnWndProc = WinFunc;
+
+  /* Register window class */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "Error", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Window creation */
+  hWnd = CreateWindow(WND_CLASS_NAME, "Practice", WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+    NULL, NULL, hInstance, NULL);
+
+  ShowWindow(hWnd, CmdShow);
+  
+  /* Message loop */
+  while (GetMessage(&msg, NULL, 0, 0))
+    DispatchMessage(&msg);
+
+  return 0;
+} /* End of 'WinMain' func */
+
+/* Toggle window fullscreen mode function */
+VOID FlipFullScreen( HWND hWnd )
+{
+  static BOOL IsFullScreen = FALSE;
+  static RECT SaveRect;
+
+  if (!IsFullScreen)
+  {
+    HMONITOR hmon;
+    MONITORINFO moninfo;
+    RECT rc;
+
+    /* Goto fullscreen mode */
+    GetWindowRect(hWnd, &SaveRect);
+
+    /* Get nearest monitor */
+    hmon = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+
+    /* Get monitor information */
+    moninfo.cbSize = sizeof(moninfo);
+    GetMonitorInfo(hmon, &moninfo);
+
+    rc = moninfo.rcMonitor;
+    AdjustWindowRect(&rc, GetWindowLong(hWnd, GWL_STYLE), FALSE);
+    SetWindowPos(hWnd, HWND_TOP,
+      rc.left, rc.top,
+      rc.right - rc.left, rc.bottom - rc.top,
+      SWP_NOOWNERZORDER);
+  }
+  else
+  {
+    /* Restore window size and position */
+    SetWindowPos(hWnd, HWND_TOP,
+      SaveRect.left, SaveRect.top,
+      SaveRect.right - SaveRect.left, SaveRect.bottom - SaveRect.top,
+      SWP_NOOWNERZORDER);
+  }
+  IsFullScreen = !IsFullScreen;
+} /* End of 'FlipFullScreen' function */
+
+
+/* Main window handle func */
+LRESULT CALLBACK WinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
+{
+  HDC hDC;
+  static int w, h;
+  static HDC hMemDC;
+  static HBITMAP hBm;
+  SYSTEMTIME st;
+  PAINTSTRUCT ps;
+  INT x, y;
+  
+  switch(Msg)
+  {
+  case WM_GETMINMAXINFO:
+    ((MINMAXINFO *)lParam)->ptMaxTrackSize.y =
+      GetSystemMetrics(SM_CYMAXTRACK) + GetSystemMetrics(SM_CYCAPTION) + 2 * GetSystemMetrics(SM_CYBORDER);
+    return 0;
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+    SelectObject(hMemDC, hBm);
+
+    /* set globe coordinates */
+    GlobeSet(w / 2, h / 2, (w < h ? w : h) * 0.4);
+
+    SendMessage(hWnd, WM_TIMER, 0, 0);
+    return 0;
+  case WM_CREATE:
+    SetTimer(hWnd, 30, 60, NULL);
+    hDC = GetDC(hWnd);
+    hMemDC = CreateCompatibleDC(hDC);
+    ReleaseDC(hWnd, hDC);
+    hBm = NULL;
+    return 0;
+  case WM_SYSKEYDOWN:
+    if (wParam == VK_RETURN)
+      FlipFullScreen(hWnd);
+    return 0;
+  case WM_KEYDOWN:
+    if (wParam == VK_ESCAPE)
+      SendMessage(hWnd, WM_CLOSE, 0, 0);
+    return 0;
+  case WM_TIMER:
+    /* cleaning background */
+    SelectObject(hMemDC, GetStockObject(WHITE_BRUSH));
+    SelectObject(hMemDC, GetStockObject(NULL_PEN));
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1); 
+
+    /* draw globe */
+    
+    GlobeDraw(hMemDC);
+
+    /* sent repaint msg */
+    InvalidateRect(hWnd, NULL, FALSE);
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+
+    /* copy frame to window */
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+
+    EndPaint(hWnd, &ps);
+    return 0;
+  case WM_CLOSE:
+    if(MessageBox(hWnd, "Are you sure you want to close?", "Close", 
+        MB_YESNO | MB_ICONQUESTION) == IDYES)
+      DestroyWindow(hWnd);
+    return 0;
+  case WM_DESTROY:
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    DeleteDC(hMemDC);
+
+    PostMessage(hWnd, WM_QUIT, 0, 0);
+    KillTimer(hWnd, 30);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+}/* End of 'WinFunc' func */
+
+/* END OF 'T01FWIN.C' FILE */
